0ea9c67ca704c47c76019957ed4bdfd3dc875e1e
Piotr <pitgal@gmail.com>
2023-11-28 00:35:21 +0100

    tabulation from spaces to tabs

--- src/sound.py
+++ src/sound.py
@@ -2,9 +2,9 @@ import pygame
 
 class Sound:
 
-    def __init__(self, path):
-        self.path = path
-        self.sound = pygame.mixer.Sound(path)
+	def __init__(self, path):
+		self.path = path
+		self.sound = pygame.mixer.Sound(path)
 
-    def play(self):
-        pygame.mixer.Sound.play(self.sound)
+	def play(self):
+		pygame.mixer.Sound.play(self.sound)
